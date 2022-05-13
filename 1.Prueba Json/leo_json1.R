52a82f4c0825d2b00639a710e3d764aa3d213033
antoniol00 <antoniolara2000@uma.es>
2022-05-13 12:39:54 +0200

    Update leo_json1.R

--- 1.Prueba Json/leo_json1.R
+++ 1.Prueba Json/leo_json1.R
@@ -3,49 +3,14 @@ library(curl)
 library(tidyjson)
 library(dplyr)
 
-file <- "0ae56408c62a25c828a3c304e30328c162c5382965ce1507f16543cb399bb971.json"
-#json_data <- fromJSON(txt = file)
-#View(json_data)
+file <- "Android/0ae56408c62a25c828a3c304e30328c162c5382965ce1507f16543cb399bb971.json"
 
+json_data <- read_json(file)
 
-json_data <- fromJSON(file) 
-glimpse(json_data)
+item <- json_data %>% spread_all()
 
-json_data %>% json_lengths()
+json_data %>% gather_object %>% json_types %>% count(name, type)
 
-json_data %>%  json_complexity()
-
-
-
-
-json_data1 <- read_json(file)
-
-glimpse(json_data1)
-
-
-t1 <- json_data1 %>% gather_object
-t1  
-
-tbl <- json_data1 %>%
-  spread_all()
-tbl
-
-
-
-json_data1 %>% json_lengths()
-
- 
-
-json_data1 %>%  json_complexity()
-
-
-json_data1 %>% gather_object %>% json_types %>% count(name, type)
-
-
-# https://github.com/colearendt/tidyjson
-# https://cran.microsoft.com/snapshot/2017-08-01/web/packages/tidyjson/vignettes/introduction-to-tidyjson.html
-# https://rdrr.io/cran/tidyjson/f/vignettes/introduction-to-tidyjson.Rmd
-# https://hendrikvanb.gitlab.io/2018/07/nested_data-json_to_tibble/
-
+colnames(item) # Vemos toda los atributos de cada fichero
 
  
