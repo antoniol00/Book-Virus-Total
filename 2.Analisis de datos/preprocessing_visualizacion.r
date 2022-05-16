71bed32083d01a3c3ef95afc596e6365561cd742
antoniol00 <antoniolara2000@uma.es>
2022-05-16 18:45:25 +0200

    tarea2

---
+++ 2.Analisis de datos/preprocessing_visualizacion.r
@@ -0,0 +1,23 @@
+library(readr)
+library(magrittr)
+library(dplyr)
+library(ggplot2)
+android <- read_csv("Book-Virus-Total/android.csv")
+
+colnames(android)
+
+#Vamos a quedarnos con las columnas más importantes
+
+android <- android %>%
+  select(scan_date,first_seen,last_seen,total,size,
+         times_submitted,harmless_votes,malicious_votes,positives,
+         submission.submitter_country,additional_info.exiftool.FileType,
+         additional_info.androguard.TargetSdkVersion,
+         contains(".result"))
+
+write.csv(android,file="android-simplified.csv")
+
+# Visualizamos datos
+plot(android %>%
+       select(scan_date,first_seen,last_seen,total,size,
+     times_submitted,harmless_votes,malicious_votes,positives))
